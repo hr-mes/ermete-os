f3fa678c98745b372e50e076e49145904e303fa8
ermete <ermete@customer.mlnnita1.isp.starlink.com>
2026-08-03 23:12:23 +0200

    feat(ui): implement unified 'ermete glass' design system crate

---
+++ forge/specs/ermete-style/src/lib.rs
@@ -0,0 +1,15 @@
+use gtk::{gdk, prelude::*};
+
+pub fn load_glass_theme() {
+    let css_provider = gtk::CssProvider::new();
+    let css = include_str!("style.css");
+    css_provider.load_from_string(css);
+
+    if let Some(display) = gdk::Display::default() {
+        gtk::style_context_add_provider_for_display(
+            &display,
+            &css_provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+    }
+}
