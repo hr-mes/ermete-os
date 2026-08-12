d21db181e1ec141b35fab9b2f319ae829a573519
ermete <ermete@customer.mlnnita1.isp.starlink.com>
2026-08-12 22:35:28 +0200

    fix(ebpf-loader): resolve needless borrow warning in build.rs

--- system/ebpf/ebpf-loader/build.rs
+++ system/ebpf/ebpf-loader/build.rs
@@ -9,14 +9,14 @@ fn main() {
         if let Some(p) = debug_path.parent() {
             let _ = fs::create_dir_all(p);
         }
-        let _ = fs::write(debug_path, &[]);
+        let _ = fs::write(debug_path, "");
     }
     
     if !release_path.exists() {
         if let Some(p) = release_path.parent() {
             let _ = fs::create_dir_all(p);
         }
-        let _ = fs::write(release_path, &[]);
+        let _ = fs::write(release_path, "");
     }
     
     println!("cargo:rerun-if-changed=../../target/bpfel-unknown-none/debug/ebpf-core");
