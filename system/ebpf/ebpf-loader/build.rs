80b0ac23ec326d84f75473bb20abd30b8965a097
ermete <ermete@customer.mlnnita1.isp.starlink.com>
2026-08-12 22:45:54 +0200

    fix(ebpf-loader): correct relative path in build.rs

--- system/ebpf/ebpf-loader/build.rs
+++ system/ebpf/ebpf-loader/build.rs
@@ -2,8 +2,8 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let debug_path = Path::new("../../target/bpfel-unknown-none/debug/ebpf-core");
-    let release_path = Path::new("../../target/bpfel-unknown-none/release/ebpf-core");
+    let debug_path = Path::new("../target/bpfel-unknown-none/debug/ebpf-core");
+    let release_path = Path::new("../target/bpfel-unknown-none/release/ebpf-core");
 
     if !debug_path.exists() {
         if let Some(p) = debug_path.parent() {
@@ -19,6 +19,6 @@ fn main() {
         let _ = fs::write(release_path, "");
     }
     
-    println!("cargo:rerun-if-changed=../../target/bpfel-unknown-none/debug/ebpf-core");
-    println!("cargo:rerun-if-changed=../../target/bpfel-unknown-none/release/ebpf-core");
+    println!("cargo:rerun-if-changed=../target/bpfel-unknown-none/debug/ebpf-core");
+    println!("cargo:rerun-if-changed=../target/bpfel-unknown-none/release/ebpf-core");
 }
